4c49659406fd074f0e2fbd09071323a648ed0799
J. Fritz Barnes <fritz@stonevalleypartners.com>
2016-12-10 13:47:41 -0800

    add podcast source layout
    * use tabs to support the source or my-podcast tabs
    * layout for a list of all source podcasts + a details view
      * support independent scrolling for each view
    * input + button for adding new podcasts

---
+++ webapp/src/app/source.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rss-sources',
+  templateUrl: './source.component.html',
+  styleUrls: ['./source.component.css'],
+})
+
+export class SourceComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+}
